dbae85384e14db32de0a559e79ce5d0c8b974f89
jsomin <1344359061@qq.com>
2022-06-27 15:15:43 +0800

    封装请求函数

--- pages/home_music/index.js
+++ pages/home_music/index.js
@@ -1,79 +1,54 @@
 // pages/home_music/index.js
+import { getTopMVs } from "../../service/api_video";
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    topMVs:[]
+    topMVs: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad:function(options) {
-    wx.request({
-      url: 'http://123.207.32.32:9001/top/mv',
-      data:{
-        offset:0,
-        limit:10
-      },
-      success:(res)=>{
-        console.log(res);
-        this.setData({ topMVs: res.data.data})
-      },
-      fail:function(err){
-        console.log(err)
-      }
-    })
+  onLoad: function (options) {
+    getTopMVs(0).then((res) => {
+      this.setData({ topMVs: res.data.data });
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
---
+++ service/api_video.js
@@ -0,0 +1,7 @@
+import hyRequest from "./index";
+export function getTopMVs(offset, limit = 10) {
+  return hyRequest.get("/top/mv", {
+    offset: offset,
+    limit: limit,
+  });
+}
